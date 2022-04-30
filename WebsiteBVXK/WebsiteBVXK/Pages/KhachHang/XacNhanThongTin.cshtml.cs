fe644b0269590b9dbbca52e09eb491534f46f5ba
yami-newbie <68190399+yami-newbie@users.noreply.github.com>
2022-04-30 12:29:29 +0700

    last-commit

--- WebsiteBVXK/WebsiteBVXK/Pages/KhachHang/XacNhanThongTin.cshtml.cs
+++ WebsiteBVXK/WebsiteBVXK/Pages/KhachHang/XacNhanThongTin.cshtml.cs
@@ -1,6 +1,8 @@
-﻿using BVXK.Domain.Enums;
+﻿using BVXK.Application.CreateDonHang;
+using BVXK.Domain.Enums;
 using BVXK.Domain.Infrastructure;
 using BVXK.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
@@ -18,6 +20,11 @@ namespace WebsiteBVXK.Pages.KhachHang
         public string loaiXe { get; set; }
         public string chuyen { get; set; }
         public string ngayDi { get; set; }
+        public string gioDi { get; set; }
+        public string noiDon { get; set; }
+        public string noiTra { get; set; }
+        public CreateDonHang.Request request { get; set; }
+        public DonHang donHang { get; set; }
         public ICustomerManager _customerManager;
         public IDonHangManager _donHangManager;
         public ITicketManager _ticketManager;
@@ -47,7 +54,45 @@ namespace WebsiteBVXK.Pages.KhachHang
             chuyen = lichtrinh.NoiXuatPhat + " -- "
                 + lichtrinh.NoiDen;
             ngayDi = lichtrinh.NgayDi.GetValueOrDefault().ToString("dd/MM/yyyy");
+            gioDi = lichtrinh.NgayDi.GetValueOrDefault().ToString("hh:mm");
             tongtien = (int)(ticket.GiaVe * seats.Count);
+            noiDon = lichtrinh.NoiXuatPhat;
+            noiTra = lichtrinh.NoiDen;
+
+            request = new CreateDonHang.Request
+            {
+                NgayDon = ngayDi,
+                Cmnd = customer.id,
+                TenKhachHang = customer.name,
+                Email = customer.email,
+                SoDienThoai = customer.phone,
+                TinhTrang = "Đã thanh toán",
+                DiemDon = noiDon,
+                DiemTra = noiTra,
+                SoLuong = seats.Count,
+                SoGhes = seats,
+                IdVeXe = ticket.IdVe,
+            };
+
+            donHang = new DonHang
+            {
+                IdVeXe = request.IdVeXe,
+                TenKhachHang = request.TenKhachHang,
+                SoDienThoai = request.SoDienThoai,
+                DiemDon = request.DiemDon,
+                DiemTra = request.DiemTra,
+                TinhTrang = request.TinhTrang == "Chưa thanh toán" ? 0 : 1,
+                ThoiGianDon = DateTime.Parse(request.NgayDon + " " + request.GioDon),
+                Email = request.Email,
+                Cmnd = request.Cmnd,
+                GhiChu = request.GhiChu,
+                SoLuong = request.SoLuong,
+            };
+        }
+
+        public async Task OnPost()
+        {
+            await _donHangManager.CreateDonHang(donHang);
         }
     }
 }
