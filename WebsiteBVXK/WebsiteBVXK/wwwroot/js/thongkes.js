9c1509f2991876df110cd635b228cabeb29fa0c7
yami-newbie <68190399+yami-newbie@users.noreply.github.com>
2022-05-04 00:57:24 +0700

    quick-fix

--- WebsiteBVXK/WebsiteBVXK/wwwroot/js/thongkes.js
+++ WebsiteBVXK/WebsiteBVXK/wwwroot/js/thongkes.js
@@ -1,7 +1,23 @@
 ﻿
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
 
-document.getElementById("den-ngay").max = new Date().toISOString().split("T")[0];
-document.getElementById("tu-ngay").max = new Date().toISOString().split("T")[0];
+if (dd < 10) {
+    dd = '0' + dd;
+}
+
+if (mm < 10) {
+    mm = '0' + mm;
+}
+
+today = yyyy + '-' + mm + '-' + dd;
+
+document.getElementById("den-ngay").setAttribute("max", today);
+document.getElementById("tu-ngay").setAttribute("max", today);
+
+
 
 var app = new Vue({
     el: "#thong-ke-doanh-thu",
